229646ddd90ede4e6d1bdaa448ce2448d7e87c0d
JulianToppo <juliantoppo95@gmail.com>
2024-05-14 17:45:54 +0530

    Homepage introduction alignment fixed

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import "./Introduction.css";
 
 const Introduction = () => {
   return (
-    <div>Introduction</div>
-  )
-}
+    <div className="intro-body">
+      <div className="intro-section1">
+        <h1><strong>Reduce</strong> <br/>Energy Consumption</h1>
+        <p>
+          Clairco provides patented technologies that enhance and provide real
+          time insights to increase energy efficiency & improve air quality
+        </p>
+        <button className="book-button">Book Demo</button>
+      </div>
+      <div className="intro-section2">
+        <img src="./images/building.png"></img>
+        <div className="benefits">
+          <div>
+            <text>-90%</text>
+            <p>in average PM   concentrations</p>
+          </div>
 
-export default Introduction
+          <div>
+            <text>12%</text>
+            <p>increase in productivity</p>
+          </div>
+
+          <div>
+            <text>25%</text>
+            <p>Energy optimization</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Introduction;
